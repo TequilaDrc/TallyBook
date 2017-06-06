1d9740c891eeb590e92891e5d53f9599a1a9b4c5
Tequila <347218294@qq.com>
2017-06-06 11:15:06 +0800

    更新

--- app/src/main/java/com/tequila/tallybook/activity/StartActivity.java
+++ app/src/main/java/com/tequila/tallybook/activity/StartActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 import com.tequila.tallybook.R;
+import com.tequila.tallybook.utils.Preference;
 
 /**
  * Created by tequila on 2017/5/3.
@@ -23,8 +25,17 @@ public class StartActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(StartActivity.this, LoginActivity.class));
-                finish();
+
+                String loginName = Preference.getInstance(StartActivity.this).getLoginName();
+                String passwd = Preference.getInstance(StartActivity.this).getPassword();
+
+                if ((!TextUtils.isEmpty(loginName)) && (!TextUtils.isEmpty(passwd))) {
+                    startActivity(new Intent(StartActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                    finish();
+                }
             }
         }, 1000);
     }
